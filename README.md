2e50271e9b965a624c29325e2bb28cc668c7e90d
Allison <allissonchervenski5@gmail.com>
2021-08-17 18:12:55 -0300

    microsserviços de propriedade e condicao

--- README.md
+++
@@ -1,2 +0,0 @@
-# RPV-Pampacare
-modernização de uma aplicação legada.
--- etl/src/main/java/com/rp5/etl/repository/ProprietarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rp5.etl.repository;
-
-import com.rp5.etl.model.Proprietario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
-}
